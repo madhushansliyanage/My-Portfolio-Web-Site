49b448daea1320f072053008fd2a25af66b812c6
Madhushan S Liyanage <127401521+madhushansliyanage@users.noreply.github.com>
2025-02-27 23:10:21 +0530

    Remove unused images and scripts; add project data and update project display logic

---
+++ projects.js
@@ -0,0 +1,33 @@
+const project1 = {
+    title: "Train Ticket Booking System",
+    id: "ttbs",
+    image: ["ttbs1.png", "ttbs2.png", "ttbs3.png"],
+    description: `Contributed to a Train Ticket Booking System, developed using the C language, collaborating with several colleagues. 
+        This system allows users to book train tickets and manage bookings with various features.
+        This project strengthened my skills in C programming, problem-solving, and collaborative software development.
+        Key Features are
+        User Registration & Login,
+        Train Schedule Viewing,
+        Ticket Price Calculation,
+        Ticket Booking,
+        Parcel Booking & Pricing,
+        Booking Deletion,
+        Payment Processing.`,
+    link: `https://github.com/madhushansliyanage/Train-Ticket-booking-system-in-C`
+}
+
+const project2 = {
+    title: "Job Advert Website",
+    id: "jaw",
+    image: [],
+    description: `Developed a Job Advert Website as a group project to showcase skills in HTML, CSS, and JavaScript. 
+    The website allows users to browse job listings with a structured UI. 
+    The most exciting part was designing and implementing a hardcoded side panel from scratch by me, which strengthened my understanding of frontend development and UI structuring. 
+    This project also improved my skills in UI design and collaboration.
+    Technologies Used: HTML, CSS, JavaScript`,
+    link: `https://github.com/madhushansliyanage/Jobs-Advert-site`
+}
+
+const projectArray = new Array(project1, project2);
+
+export { projectArray };
